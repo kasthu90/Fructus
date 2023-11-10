4cf1e1eeaa31bf94d33545568ce268e0fc602abe
Kasthuri <tkasthu.90@gmail.com>
2023-11-10 14:22:44 +1100

    Create data model and fetch data

--- Fructus/View/FruitCardView.swift
+++ Fructus/View/FruitCardView.swift
@@ -11,43 +11,45 @@ struct FruitCardView: View {
     
     //Properties
     @State private var isAnimating: Bool = false
+    var fruit: Fruit
+    
     var body: some View {
         ZStack {
             VStack(spacing: 20) {
-                Image("blueberry")
+                Image(fruit.image)
                     .resizable()
                     .scaledToFit()
                     .shadow(color: Color(red:0 , green: 0,blue: 0,opacity: 0.15), radius: 8, x:6, y:8)
                     .scaleEffect(isAnimating ? 1: 0)
-                Text("Blueberry")
+                Text(fruit.title)
                     .foregroundColor(.white)
                     .font(.largeTitle)
                     .fontWeight(.heavy)
                     .shadow(color: Color(red:0, green: 0, blue: 0, opacity: 0.15), radius: 2, x: 2, y: 2)
-                Text("Bluberries are sweet, nutrious and widely popular all over the world")
+                Text(fruit.headline)
                     .foregroundColor(Color.white)
                     .multilineTextAlignment(.center)
                     .padding(.horizontal,16)
                     .frame(maxWidth: 480)
                 
                 StartButtonView()
-               
+                
             }
         }
         .onAppear{
             withAnimation(.easeOut(duration: 0.5))
-                          {
+            {
                 isAnimating = true
             }
-            }
+        }
         .frame(minWidth: /*@START_MENU_TOKEN@*/0/*@END_MENU_TOKEN@*/, maxWidth: .infinity, minHeight: 0, maxHeight: .infinity, alignment: .center)
-        .background(LinearGradient(gradient: Gradient(colors: [Color("ColorBlueBerryLight"), Color("ColorBlueBerryDark")]), startPoint: .top, endPoint: .bottom))
+        .background(LinearGradient(gradient: Gradient(colors: fruit.gradientColors), startPoint: .top, endPoint: .bottom))
         .cornerRadius(20)
         .padding(.horizontal,20)
     }
 }
 
 #Preview {
-    FruitCardView()
+    FruitCardView(fruit: fruitsData[1])
         .previewLayout(.fixed(width: 320, height: 640))
 }
